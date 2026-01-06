d8741e607df6a4f68fc7f52e58fc2176b87179ab
nirbhay-design <sharma.59@iitj.ac.in>
2026-01-06 20:04:34 +0530

    experiments with different parameters with scalre

--- test_run.sh
+++ test_run.sh
@@ -8,13 +8,13 @@
 
 # nohup python train.py --config configs/lema.c100.yaml --gpu 6 --model resnet18 --save_path lema.c100.r18.v3.pth --epochs 350 --epochs_lin 100 --test --tsne --knn --lreg > test_garbage/lema.c100.r18.v3.log &
 
-nohup python train.py --config configs/scalre.c10.yaml --gpu 0 --model resnet50 --save_path scalre.c10.r50.pth --epochs 350 --epochs_lin 100 --net_type energy --test --tsne --knn --lreg > test_garbage/scalre.c10.r50.log &
+# nohup python train.py --config configs/scalre.c10.yaml --gpu 0 --model resnet50 --save_path scalre.c10.r50.pth --epochs 350 --epochs_lin 100 --net_type energy --test --tsne --knn --lreg > test_garbage/scalre.c10.r50.log &
 
-nohup python train.py --config configs/scalre.c10.yaml --gpu 0 --model resnet18 --save_path scalre.c10.r18.pth --epochs 350 --epochs_lin 100 --net_type energy --test --tsne --knn --lreg > test_garbage/scalre.c10.r18.log &
+# nohup python train.py --config configs/scalre.c10.yaml --gpu 0 --model resnet18 --save_path scalre.c10.r18.pth --epochs 350 --epochs_lin 100 --net_type energy --test --tsne --knn --lreg > test_garbage/scalre.c10.r18.log &
 
 # nohup python train.py --config configs/scalre.c100.yaml --gpu 7 --model resnet50 --save_path scalre.c100.r50.pth --epochs 350 --epochs_lin 100 --net_type energy --test --tsne --knn --lreg > test_garbage/scalre.c100.r50.log &
 
-nohup python train.py --config configs/scalre.c100.yaml --gpu 1 --model resnet18 --save_path scalre.c100.r18.pth --epochs 350 --epochs_lin 100 --net_type energy --test --tsne --knn --lreg > test_garbage/scalre.c100.r18.log &
+# nohup python train.py --config configs/scalre.c100.yaml --gpu 1 --model resnet18 --save_path scalre.c100.r18.pth --epochs 350 --epochs_lin 100 --net_type energy --test --tsne --knn --lreg > test_garbage/scalre.c100.r18.log &
 
 # nohup python train.py --config configs/scalre.c10.yaml --gpu 7 --model resnet50 --save_path scalre.c10.r50.sc.pth --epochs 350 --epochs_lin 100 --net_type score --test --tsne --knn --lreg > test_garbage/scalre.c10.r50.sc.log &
 
@@ -23,3 +23,28 @@ nohup python train.py --config configs/scalre.c100.yaml --gpu 1 --model resnet18
 # nohup python train.py --config configs/scalre.c100.yaml --gpu 7 --model resnet50 --save_path scalre.c100.r50.sc.pth --epochs 350 --epochs_lin 100 --net_type score --test --tsne --knn --lreg > test_garbage/scalre.c100.r50.log &
 
 # nohup python train.py --config configs/scalre.c100.yaml --gpu 7 --model resnet18 --save_path scalre.c100.r18.sc.pth --epochs 350 --epochs_lin 100 --net_type score --test --tsne --knn --lreg > test_garbage/scalre.c100.r18.log &
+
+
+# nohup python train.py --config configs/lema.c10.yaml --gpu 0 --model resnet50 --save_path lema.c10.r50.v3.e800.lin.pth --epochs 800 --epochs_lin 100 --mlp_type linear --linear_lr 0.1 > logs/lema.c10.r50.v3.e800.lin.log &
+
+# nohup python train.py --config configs/lema.c10.yaml --gpu 0 --model resnet18 --save_path lema.c10.r18.v3.e800.lin.pth --epochs 800 --epochs_lin 100 --mlp_type linear --linear_lr 0.1 > logs/lema.c10.r18.v3.e800.lin.log &
+
+# nohup python train.py --config configs/lema.c100.yaml --gpu 1 --model resnet50 --save_path lema.c100.r50.v3.e800.lin.pth --epochs 800 --epochs_lin 100 --mlp_type linear --linear_lr 0.1 > logs/lema.c100.r50.v3.e800.lin.log &
+
+# nohup python train.py --config configs/lema.c100.yaml --gpu 1 --model resnet18 --save_path lema.c100.r18.v3.e800.lin.pth --epochs 800 --epochs_lin 100 --mlp_type linear --linear_lr 0.1 > logs/lema.c100.r18.v3.e800.lin.log &
+
+# nohup python train.py --config configs/scalre.c10.yaml --gpu 2 --model resnet50 --save_path scalre.c10.r50.e800.lin0.1.pth --epochs 800 --epochs_lin 100 --mlp_type linear --linear_lr 0.1 --net_type energy > logs/scalre.c10.r50.e800.lin0.1.log &
+
+# nohup python train.py --config configs/scalre.c10.yaml --gpu 3 --model resnet18 --save_path scalre.c10.r18.e800.lin0.1.pth --epochs 800 --epochs_lin 100 --mlp_type linear --linear_lr 0.1 --net_type energy > logs/scalre.c10.r18.e800.lin0.1.log &
+
+# nohup python train.py --config configs/scalre.c100.yaml --gpu 4 --model resnet50 --save_path scalre.c100.r50.e800.lin0.1.pth --epochs 800 --epochs_lin 100 --mlp_type linear --linear_lr 0.1 --net_type energy > logs/scalre.c100.r50.e800.lin0.1.log &
+
+# nohup python train.py --config configs/scalre.c100.yaml --gpu 3 --model resnet18 --save_path scalre.c100.r18.e800.lin0.1.pth --epochs 800 --epochs_lin 100 --mlp_type linear --linear_lr 0.1 --net_type energy > logs/scalre.c100.r18.e800.lin0.1.log &
+
+nohup python train.py --config configs/scalre.c10.yaml --gpu 4 --model resnet50 --save_path scalre.c10.r50.sc.e800.lin0.1.pth --epochs 800 --epochs_lin 100 --mlp_type linear --linear_lr 0.1 --net_type score > logs/scalre.c10.r50.sc.e800.lin0.1.log &
+
+nohup python train.py --config configs/scalre.c10.yaml --gpu 4 --model resnet18 --save_path scalre.c10.r18.sc.e800.lin0.1.pth --epochs 800 --epochs_lin 100 --mlp_type linear --linear_lr 0.1 --net_type score > logs/scalre.c10.r18.sc.e800.lin0.1.log &
+
+nohup python train.py --config configs/scalre.c100.yaml --gpu 5 --model resnet50 --save_path scalre.c100.r50.sc.e800.lin0.1.pth --epochs 800 --epochs_lin 100 --mlp_type linear --linear_lr 0.1 --net_type score > logs/scalre.c100.r50.sc.e800.lin0.1.log &
+
+nohup python train.py --config configs/scalre.c100.yaml --gpu 5 --model resnet18 --save_path scalre.c100.r18.sc.e800.lin0.1.pth --epochs 800 --epochs_lin 100 --mlp_type linear --linear_lr 0.1 --net_type score > logs/scalre.c100.r18.sc.e800.lin0.1.log &
