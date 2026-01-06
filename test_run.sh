70a5c195a8b9a873dd120702fe7927b142e2973a
nirbhay-design <sharma.59@iitj.ac.in>
2026-01-07 00:16:09 +0530

    running score scalre for 800 epochs

--- test_run.sh
+++ test_run.sh
@@ -41,10 +41,10 @@
 
 # nohup python train.py --config configs/scalre.c100.yaml --gpu 3 --model resnet18 --save_path scalre.c100.r18.e800.lin0.1.pth --epochs 800 --epochs_lin 100 --mlp_type linear --linear_lr 0.1 --net_type energy > logs/scalre.c100.r18.e800.lin0.1.log &
 
-nohup python train.py --config configs/scalre.c10.yaml --gpu 4 --model resnet50 --save_path scalre.c10.r50.sc.e800.lin0.1.pth --epochs 800 --epochs_lin 100 --mlp_type linear --linear_lr 0.1 --net_type score > logs/scalre.c10.r50.sc.e800.lin0.1.log &
+nohup python train.py --config configs/scalre.c10.yaml --gpu 1 --model resnet50 --save_path scalre.c10.r50.sc.e800.lin0.1.pth --epochs 800 --epochs_lin 100 --mlp_type linear --linear_lr 0.1 --net_type score > logs/scalre.c10.r50.sc.e800.lin0.1.log &
 
-nohup python train.py --config configs/scalre.c10.yaml --gpu 4 --model resnet18 --save_path scalre.c10.r18.sc.e800.lin0.1.pth --epochs 800 --epochs_lin 100 --mlp_type linear --linear_lr 0.1 --net_type score > logs/scalre.c10.r18.sc.e800.lin0.1.log &
+nohup python train.py --config configs/scalre.c10.yaml --gpu 0 --model resnet18 --save_path scalre.c10.r18.sc.e800.lin0.1.pth --epochs 800 --epochs_lin 100 --mlp_type linear --linear_lr 0.1 --net_type score > logs/scalre.c10.r18.sc.e800.lin0.1.log &
 
-nohup python train.py --config configs/scalre.c100.yaml --gpu 5 --model resnet50 --save_path scalre.c100.r50.sc.e800.lin0.1.pth --epochs 800 --epochs_lin 100 --mlp_type linear --linear_lr 0.1 --net_type score > logs/scalre.c100.r50.sc.e800.lin0.1.log &
+nohup python train.py --config configs/scalre.c100.yaml --gpu 7 --model resnet50 --save_path scalre.c100.r50.sc.e800.lin0.1.pth --epochs 800 --epochs_lin 100 --mlp_type linear --linear_lr 0.1 --net_type score > logs/scalre.c100.r50.sc.e800.lin0.1.log &
 
-nohup python train.py --config configs/scalre.c100.yaml --gpu 5 --model resnet18 --save_path scalre.c100.r18.sc.e800.lin0.1.pth --epochs 800 --epochs_lin 100 --mlp_type linear --linear_lr 0.1 --net_type score > logs/scalre.c100.r18.sc.e800.lin0.1.log &
+nohup python train.py --config configs/scalre.c100.yaml --gpu 0 --model resnet18 --save_path scalre.c100.r18.sc.e800.lin0.1.pth --epochs 800 --epochs_lin 100 --mlp_type linear --linear_lr 0.1 --net_type score > logs/scalre.c100.r18.sc.e800.lin0.1.log &
